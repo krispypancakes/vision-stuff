511d03d33af6a157214e50d03121f0357948089b
phthomsen <pthomsen.ml@gmail.com>
2024-02-14 21:51:18 +0100

    outsource estimate-loss

--- helpers.py
+++ helpers.py
@@ -1,4 +1,7 @@
 """ useful helpers """
+import torch
+import numpy as np
+
 
 def get_model_size(model):
   param_size = 0
@@ -7,8 +10,15 @@ def get_model_size(model):
   buffer_size = 0
   for buffer in model.buffers():
       buffer_size += buffer.nelement() * buffer.element_size()
-
   size_all_mb = (param_size + buffer_size) / 1024**2
   print('model size: {:.3f}MB'.format(size_all_mb))
-
   return size_all_mb
+
+@torch.no_grad()
+def estimate_loss(model, loader, crit):
+  test_loss = []
+  for x, y in loader:
+    predictions = model(x)
+    loss = crit(predictions, y)
+    test_loss.append(loss.item())
+  return np.mean(test_loss)
