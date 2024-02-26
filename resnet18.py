37dc3043268d8a247ce87a3fc4dc5aa6c11b9d69
phthomsen <pthomsen.ml@gmail.com>
2024-02-26 18:49:42 +0100

    hyper hyper

--- resnet18.py
+++ resnet18.py
@@ -6,103 +6,22 @@ import torch.optim as optim
 import numpy as np
 import pickle
 from tqdm import trange
-import matplotlib.pyplot as plt
 import time
 from helpers import get_model_size, estimate_loss, CiFaData
-
-
-class PrepBlock(nn.Module):
-  # fixed channels for cifar
-  def __init__(self):
-    super().__init__()
-    self.prep_block = nn.Sequential(
-      nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False),
-      nn.BatchNorm2d(64),
-      nn.ReLU(),
-      nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-    )
-  def forward(self, x):
-    return self.prep_block(x)
-  def init_weights(self):
-    for layer in self.prep_block:
-      if isinstance(layer, nn.Conv2d):
-        nn.init.kaiming_normal_(layer.weight, mode='fan_out', nonlinearity='relu')
-        if layer.bias is not None:
-          layer.bias.data.zero_()
-
-class ComputeBlock(nn.Module):
-  def __init__(self, inchannels, outchannels, stride, downsample=None):
-    super().__init__()
-    self.convblock = nn.Sequential(
-      nn.Conv2d(in_channels=inchannels, out_channels=outchannels, kernel_size=3, padding=1, stride=stride, bias=False),
-      nn.BatchNorm2d(outchannels),
-      nn.ReLU(),
-      nn.Conv2d(in_channels=outchannels, out_channels=outchannels, kernel_size=3, padding=1, stride=1, bias=False),
-      nn.BatchNorm2d(outchannels)
-    )
-    self.downsample = downsample
-    if not inchannels == outchannels:
-      self.downsample = nn.Sequential(
-        nn.Conv2d(in_channels=inchannels, out_channels=outchannels, kernel_size=1, stride=2, bias=False),
-        nn.BatchNorm2d(outchannels)
-      )
-    self.relu = nn.ReLU()
-  def forward(self, x):
-    x_skip = x 
-    x = self.convblock(x)
-    if self.downsample:
-      x_skip = self.downsample(x_skip)
-    out = self.relu(x+x_skip)
-    return out
-  def init_weights(self):
-    for layer in self.convblock:
-      if isinstance(layer, nn.Conv2d):
-        nn.init.kaiming_normal_(layer.weight, mode='fan_out', nonlinearity='relu')
-        if layer.bias is not None:
-          layer.bias.data.zero_()
-    if self.downsample:
-      for layer in self.downsample:
-        if isinstance(layer, nn.Conv2d):
-          nn.init.kaiming_normal_(layer.weight, mode='fan_out', nonlinearity='relu')
-          if layer.bias is not None:
-            layer.bias.data.zero_()
-  
-  
-class ResNet18(nn.Module):
-  def __init__(self):
-    super().__init__()
-    self.resnet = nn.Sequential(
-      PrepBlock(), # (B,64,8,8)
-      ComputeBlock(64, 64, stride=1), # (B,64,8,8)
-      ComputeBlock(64,128, stride=2), # (B,128,4,4)
-      ComputeBlock(128,256, stride=2), # (B,256,2,2)
-      ComputeBlock(256, 512, stride=2), # (B,512,1,1)
-      nn.AdaptiveAvgPool2d((1,1)),
-      nn.Flatten(start_dim=1), # (B,512)
-      nn.Linear(512, 10) # (B, 10)
-    )
-  def forward(self, x):
-    return self.resnet(x)
-  def init_weights(self):
-    for module in self.modules():
-      if isinstance(module, ComputeBlock):
-        module.init_weights()
-      elif isinstance(module, PrepBlock):
-        module.init_weights()
-      elif isinstance(module, nn.Linear):
-        nn.init.kaiming_normal_(module.weight, mode='fan_out', nonlinearity='relu')
+from models import ResNet18
 
 
 def main():
   device = 'cuda' if torch.cuda.is_available() else 'cpu'
   epochs = 900
   batch_size = 256
-  momentum = 0.875
-  w_decay = 0.00125
+  momentum = 0.54
+  w_decay = 0.00472
   n_worksers = 14
-  lr = 0.001
+  lr = 0.0027
   tf = transforms.Compose([transforms.RandomResizedCrop((32,32)), 
-                         transforms.RandomHorizontalFlip(p=0.5)])
+                         transforms.RandomHorizontalFlip(p=0.5),
+                         ])
   # mean and std of channels of cifar
   params = torch.tensor([0.4919, 0.4827, 0.4472]), torch.tensor([0.2470, 0.2434, 0.2616])
   dataset_path = "data/"
