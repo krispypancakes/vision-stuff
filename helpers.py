d6ad7aeec6ab88cf3d2b335a6d241adb6d03e7ee
phthomsen <pthomsen.ml@gmail.com>
2024-02-26 18:49:10 +0100

    adjust loss eval ; ;

--- helpers.py
+++ helpers.py
@@ -51,20 +51,20 @@ def get_model_size(model):
   print('model size: {:.3f} MB'.format(size_all_mb))
   return size_all_mb
 
-@torch.no_grad()
+@torch.no_grad
 def estimate_loss(model, loader, crit, device):
   # torch.no_grad tells torch that there there's no grad -> tensors
   # model.eval() takes care of batchnorm or dropout
   model.eval()
-  correct, total_loss, total_count = 0, 0, 0
+  correct, total_loss, total_count, batch_count = 0, 0, 0, 0
   for x, y in loader:
     x, y = x.to(device), y.to(device)
     predictions = model(x)
-    loss = crit(predictions, y)
-    total_loss += loss.item() * x.shape[0]
+    total_loss += crit(predictions, y).item()
     total_count += x.shape[0]
+    batch_count += 1
     correct += (predictions.argmax(dim=1) == y).sum().item()
-  return total_loss/total_count, correct/total_count
+  return total_loss/batch_count, correct/total_count
 
 def get_parameters(loader):
   data = None
